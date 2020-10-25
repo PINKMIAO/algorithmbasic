e691ac68e4d946d143562041cac057e617dcb386
PINK_MIAO <1176085412@qq.com>
2020-10-25 14:20:02 +0800

    'version4'

--- src/main/java/com/baven/GitTestVersion.java
+++ src/main/java/com/baven/GitTestVersion.java
@@ -5,6 +5,7 @@ public class GitTestVersion {
     public static void main(String[] args) {
         System.out.println("version3.0");
         System.out.println("version2.0");
+        System.out.println("version4.0");
     }
 
 }

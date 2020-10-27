1245b8c655a1bcd0b596d7580685309fd15d40aa
PINK_MIAO <1176085412@qq.com>
2020-10-27 15:04:32 +0800

    'version4.0'

--- src/main/java/com/baven/Algorithm.java
+++ src/main/java/com/baven/Algorithm.java
@@ -1,8 +1,34 @@
 package com.baven;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Algorithm {
 
     public static void main(String[] args) {
+        System.out.println("version 4.0");
     }
 
+
+}
+class Solution {
+    // baoli jiefa
+    public int[] solution(int[] arr){
+        int len = arr.length;
+        List<Integer> ans = new ArrayList<Integer>();
+
+        for (int i = 0; i < len; i++) {
+            int temp = arr[i];
+            int count = 0;
+            for (int j = 0; j < len; j++) {
+                if (arr[i] > arr[j]){
+                    count++;
+                }
+            }
+            ans.add(count);
+        }
+
+        ans.toArray();
+        return null;
+    }
 }

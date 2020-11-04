a85cd2fa6349b8af7eb6660ad7fe0a11d63d54a7
PINK_MIAO <1176085412@qq.com>
2020-11-04 21:54:57 +0800

    'addCountAndRadixSort'

---
+++ src/main/java/com/baven/sort/CountSort.java
@@ -0,0 +1,51 @@
+package com.baven.sort;
+
+/**
+ * 计数排序
+ */
+public class CountSort {
+
+    public static void main(String[] args) {
+        int[] arr = createGroup(10, 10);
+        printGroup(arr);
+        countSort(arr);
+        printGroup(arr);
+    }
+
+    // util for test
+    public static int[] createGroup(int maxSize, int maxValue){
+        int[] arr = new int[(int)(Math.random() * maxSize)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * maxValue);
+        }
+        return arr;
+    }
+    public static void printGroup(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void countSort(int[] arr){
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int[] help = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            help[arr[i]]++;
+        }
+        int temp = 0;
+        for (int i = 0; i < help.length; i++) {
+            while (help[i]-- > 0) {
+                arr[temp++] = i;
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/com/baven/sort/TireTree.java
@@ -0,0 +1,12 @@
+package com.baven.sort;
+
+/**
+ * 前缀树、字典树等
+ */
+public class TireTree {
+
+    public static void main(String[] args) {
+
+    }
+
+}
